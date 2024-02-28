dab1e0eabd8a6a23beb49dc5904c325999f2d56f
SiyaNkosii <nsyabonga7@gmail.com>
2024-02-28 15:16:59 +0200

    swagger

--- Api-gateway/src/main/java/Apigateway/config/CorsConfig.java
+++ Api-gateway/src/main/java/Apigateway/config/CorsConfig.java
@@ -1,18 +1,31 @@
 package Apigateway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
+public class CorsConfig extends org.springframework.web.cors.CorsConfiguration{
+        @Bean
+        public CorsWebFilter corsFilter()
+        {
+            org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+            corsConfiguration.setAllowCredentials(true);
+            corsConfiguration.addAllowedOrigin("*");
+            corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+            corsConfiguration.addAllowedHeader("origin");
+            corsConfiguration.addAllowedHeader("content-type");
+            corsConfiguration.addAllowedHeader("accept");
+            corsConfiguration.addAllowedHeader("authorization");
+            corsConfiguration.addAllowedHeader("cookie");
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", corsConfiguration);
+            return new CorsWebFilter(source);
 
+        }
 }
